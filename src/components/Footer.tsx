5d88832f97e9524e3fa4fff0258d20cada4f660f
Oleksandr Tertyshnyk <aleksandr.tertyshnyk@gmail.com>
2022-11-10 23:41:05 +0200

    Mail send added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,14 +11,17 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
-import React from "react";
+import React, { useState } from "react";
 
 function Footer() {
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
   return (
     <AppBar
       position="static"
       color="secondary"
-      sx={{ bgcolor: "background.default"}}
+      sx={{ bgcolor: "background.default" }}
     >
       <Box display="flex" justifyContent="space-between">
         <Box margin="1rem">
@@ -30,17 +33,29 @@ function Footer() {
           autoComplete="off"
           gap={1}
           onSubmit={(e) => {
-            e.preventDefault()
-
+            e.preventDefault();
+            if (message && email) {
+              window.open(
+                `mailto:${email}?subject="countries"&body=${message}`
+              );
+            }
           }}
         >
           <TextField
+            type="text"
             label="Text of a message"
             placeholder="Dear reciver ..."
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
           ></TextField>
           <TextField
+            type="email"
             label="Your e-mail"
-            placeholder="exaple@mail.come"
+            placeholder="email@example.com"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           ></TextField>
           <Button
             sx={{ m: 1 }}
@@ -50,29 +65,16 @@ function Footer() {
           >
             Send e-mail
           </Button>
-          {/*  <a href="mailto:is@exaple.com">contact email</a>.<br /> */}
         </Box>
         <Box margin="1rem">
           <Typography>Social media</Typography>
-          <Link
-            width="50"
-            href="https://www.instagram.com/"
-            target="_blank"
-          >
+          <Link width="50" href="https://www.instagram.com/" target="_blank">
             <InstagramIcon />
           </Link>
-          <Link
-            width="50"
-            href="https://www.facebook.com/"
-            target="_blank"
-          >
+          <Link width="50" href="https://www.facebook.com/" target="_blank">
             <FacebookIcon />
           </Link>
-          <Link
-            width="50"
-            href="https://linkedin.com/"
-            target="_blank"
-          >
+          <Link width="50" href="https://linkedin.com/" target="_blank">
             <LinkedInIcon />
           </Link>
         </Box>
